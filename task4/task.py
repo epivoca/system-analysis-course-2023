968452c5a30a75e3cd0a144fa0395b3110f3195e
epivoca <artfom02@gmail.com>
2024-01-21 23:26:40 +0300

    added argparse option for the future and minor code fixes v3

--- task4/task.py
+++ task4/task.py
@@ -1,9 +1,11 @@
+from typing import Tuple
 import numpy as np
 
 
 def get_probabilities(frequency_matrix):
     probabilities = []
     n = len(frequency_matrix[0])
+    
     for line in frequency_matrix:
         result = []
         for cell in line:
@@ -16,32 +18,39 @@ def get_probabilities(frequency_matrix):
 def get_entropies(frequency_matrix):
     probability_matrix = get_probabilities(frequency_matrix)
     entropy_matrix = []
+    
     for i in range(len(frequency_matrix)):
         entropy_matrix.append([0] * len(frequency_matrix[0]))
+
     for l, line in enumerate(probability_matrix):
         for c, Plc in enumerate(line):
             Hlc = 0
             if Plc != 0: Hlc = - Plc * np.log2(Plc)
             entropy_matrix[l][c] = Hlc
+
     return entropy_matrix
 
 
-def get_Ha(frequency_matrix):
+def get_Ha(frequency_matrix) -> float:
     prob_matrix = get_probabilities(frequency_matrix)
     summ = 0
+
     for line in prob_matrix:
         p = np.sum(line)
         summ -= p * np.log2(p)
+
     return summ
 
 
-def get_Hb(frequency_matrix):
+def get_Hb(frequency_matrix) -> float:
     prob_matrix = get_probabilities(frequency_matrix)
     summ = 0
     prob_matrix_T = np.transpose(prob_matrix)
+
     for line in prob_matrix_T:
         p = np.sum(line)
         if p != 0: summ -= p * np.log2(p)
+
     return summ
 
 
@@ -49,7 +58,7 @@ def get_HAB(frequency_matrix):
     return np.sum(get_entropies(frequency_matrix))
 
 
-def task():
+def task() -> Tuple:
     matrix = []
     for i in range(2, 13):
         matrix.append([0] * 36)
@@ -65,5 +74,31 @@ def task():
     return  HAB, Ha, Hb, HaB, Iab
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--file",
+        type=str,
+        required=True,
+        help="Path for your CSV file.",
+    )
+    parser.add_argument(
+        "--x",
+        type=str,
+        required=True,
+        help="Row index."
+    )
+    parser.add_argument(
+        "--y",
+        type=str,
+        required=True,
+        help="Column index."
+    )
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args
     print(task())
